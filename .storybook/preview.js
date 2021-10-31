875d76ea7ce96ff81e0f6a844d611c7cf05a34ad
lazlanrafar <lazlanrafar@gmail.com>
2021-10-31 10:26:34 +0700

    Add Storybook Organisms

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -14,6 +14,7 @@ import '../styles/transactions.css'
 import '../styles/transactions-detail.css'
 import '../styles/edit-profile.css'
 import '../styles/navbar-log-in.css'
+import * as nextImage from 'next/image'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
